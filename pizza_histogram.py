679991e7400b0f3f65dd6528921e40c2293bc1a1
Avery <avery@Averys-MacBook-Air.local>
2021-11-11 14:51:32 -0600

    variable names changed

--- pizza_histogram.py
+++ pizza_histogram.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 
-o = [("pepperoni", 2), ("hawaiian", 7), ("pepperoni",8), ("cheese", 2), ("hawaiian", 20), ("sausage", 5), ("cheese", 10), ("bacon", 2)]
+pizzas = [("pepperoni", 2), ("hawaiian", 7), ("pepperoni",8), ("cheese", 2), ("hawaiian", 20), ("sausage", 5), ("cheese", 10), ("bacon", 2)]
 
 
 '''
 '''
-d = {}
-for i in o:
-    if i[0] in d.keys():
-        d[i[0]] += i[1]
+hist = {}
+for pizza in pizzas:
+    if pizza[0] in hist.keys():
+        hist[pizza[0]] += pizza[1]
     else:
 
-        d[i[0]] = i[1]
+        hist[pizza[0]] = pizza[1]
 
-for k, v in d.items():
+for k, v in hist.items():
     print("{}: {}".format(k, v))
 
 
-plt.bar(list(d.keys()), d.values())
+plt.bar(list(hist.keys()), hist.values())
 plt.show()
